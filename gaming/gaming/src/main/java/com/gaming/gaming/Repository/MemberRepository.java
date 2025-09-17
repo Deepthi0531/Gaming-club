ef8b7f162bb62802c9a578a2c0fc24254eb720d6
Ananyasbabu <ananyasbabu2005@gmail.com>
2025-09-17 10:33:31 +0530

    gaming project members updated

---
+++ gaming/gaming/src/main/java/com/gaming/gaming/Repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.gaming.gaming.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import com.gaming.gaming.Model.Member;
+
+@Repository
+public interface  MemberRepository extends MongoRepository<Member, String> {
+    Member findByPhone(String phone);
+}
